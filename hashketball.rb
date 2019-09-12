47126b09f008afcca7f3b8c6a95674619920752f
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-12 13:52:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -138,7 +138,8 @@ end
 def shoe_size(players_name)
   game_hash.each do |place, team|
     team[:players].each do |attribute, data|
-      if attribute == players 
+      if attribute == :players
+        
     end
   end
 end
